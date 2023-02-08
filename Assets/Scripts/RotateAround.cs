7e15001a9f1b01da2e64bde6388020cb0e943b0c
heeyeon9578 <heeyeon9578@naver.com>
2023-02-08 16:49:14 +0900

    태양계 세팅

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	[SerializeField] Transform target; 
+	public int speed; 
+	
+	void Start() {
+		if (target == null) 
+		{
+			target = gameObject.transform;
+		}
+	}
+
+	void Update () {
+
+		transform.RotateAround(target.transform.position,target.transform.up,speed * Time.deltaTime);
+	}
+}
